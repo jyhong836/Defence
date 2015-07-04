9cbf0f1f2ef01593bec6b7a8b7f07f6e356d363d
Junyuan Hong <jyhong836@gmail.com>
2015-07-04 22:53:54 +0800

    Complete some features of Enemy and Tower

--- Defence1/Assets/Scripts/HelperScripts/Masks.cs
+++ Defence1/Assets/Scripts/HelperScripts/Masks.cs
@@ -4,5 +4,6 @@ using System.Collections;
 public static class Masks {
 
 	public static readonly int Ore = LayerMask.GetMask ("Ore");
-	public static readonly int Enemy = LayerMask.GetMask ("Ore");
+	public static readonly int Enemy = LayerMask.GetMask ("Enemy");
+	public static readonly int Tower = LayerMask.GetMask ("Tower");
 }
--- Defence1/Assets/Scripts/Tower.cs
+++ Defence1/Assets/Scripts/Tower.cs
@@ -1,34 +1,84 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum TowerMode {
+	Attack, // auto resume from powerless
+	Idle,
+	AttackOnce // whill attack until out of power, not resume.
+}
+
 public class Tower : MonoBehaviour {
 	
-	public float workingRadius = 6;
+	public float attackingRadius = 6;
 	
 	[SerializeField] Enemy currentTarget;
-//	[SerializeField] bool isWorking = true;
-	private bool isAttacking = false;
-	private bool isOutOfPower = true;
-	private float power = 0;
-	public float idlePowerUsage = 1;
-	public float attackPowerUsage = 10;
+	public bool isAttacking = false;
+	public float idlePowerUsage = 0.001f; // per sec
+	public float attackPowerUsage = 0.01f; // per sec
 	public float injury = 10;
 	public float attackInterval = 2;
-	private float nextAttackTime = attackInterval;
+	private float nextAttackTime;
+
+	// mode
+	[SerializeField] TowerMode mode = TowerMode.Attack;
+	public TowerMode currentMode {
+		get {return mode;}
+		set {
+			mode = value;
+			switch (mode) 
+			{
+				case TowerMode.Attack: StartAttack(); break;
+				case TowerMode.AttackOnce: StartAttack(); break;
+				case TowerMode.Idle: break;
+//				default: Debug.Log("ERROR in Tower mode switch: Unknown Mode");
+			}
+		}
+	}
+
+	// power
+	private bool isOutOfPower = false; // TODO true;
+	[SerializeField] float power = 0;
+	public float powerLeft {
+		get { return power; }
+		set {
+			power = value;
+			if (power>0) {
+				isOutOfPower = false;
+				if (mode==TowerMode.Attack) {
+					StartAttack ();
+				}
+			} else 
+				isOutOfPower = true;
+		}
+	}
+
+	// life	
+	[SerializeField] float life = 100;
+	public float lifeLeft {
+		get {
+			return life;
+		}
+		set {
+			life = value;
+			if (life<=0) {
+				Destroy(gameObject);
+			}
+		}
+	}
 
 	void FixedUpdate () {
 		if (isAttacking) {
 			if (power < attackPowerUsage) {
 				isAttacking = false;
 			} else {
-				power -= attackPowerUsage;
+				powerLeft -= attackPowerUsage*Time.fixedDeltaTime;
 			}
 			Attack();
 		} else if (!isOutOfPower) {
-			if (power < idlePowerUsage) {
+			if (powerLeft < idlePowerUsage) {
 				isOutOfPower = true;
 			} else {
-				power -= idlePowerUsage;
+				powerLeft -= idlePowerUsage*Time.fixedDeltaTime;
 			}
 		}
 	}
@@ -36,19 +86,32 @@ public class Tower : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		ChangeCurrentTarget ();
+		nextAttackTime = attackInterval;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (currentTarget != null) {
+		if (isAttacking && currentTarget != null) {
 			var start = transform.position;
 			var end = currentTarget.transform.position;
-			
+
 			Debug.DrawLine (start,end,Color.red);
 		}
 	}
 
+	void StartAttack () {
+		if (currentTarget!=null)
+			isAttacking = true;
+		else {
+			ChangeCurrentTarget();
+		}
+	}
+
 	void Attack () {
+		if (currentTarget == null) {
+			ChangeCurrentTarget ();
+			return;
+		}
 		if (currentTarget.lifeLeft > 0)
 			AttackTarget ();
 		else
@@ -65,14 +128,16 @@ public class Tower : MonoBehaviour {
 	}
 	
 	void ChangeCurrentTarget () {
-		var colliders = Physics.OverlapSphere (transform.position,workingRadius,Masks.Enemy);
+		var colliders = Physics.OverlapSphere (transform.position,attackingRadius,Masks.Enemy);
 		if (colliders.Length > 0) {
 			//random pick one
 			var index = UnityEngine.Random.Range (0, colliders.Length);
 			var enemy = colliders [index].gameObject.GetComponent <Enemy>();
 			currentTarget = enemy;
+			isAttacking = true;
 		} else {
 			currentTarget = null;
+			isAttacking = false;
 		}
 	}
 }
