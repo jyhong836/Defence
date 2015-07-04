ebfb21c8073db8bb0e6172846735cf5f3045296a
魏家一 <wjydzh1@163.com>
2015-07-04 10:57:39 +0800

    extract the UI handling functionality from GameManager to UIManager. Also add support for tower placement cancelation. (via ESC or Right Mouse Button -- "Cancel" in Input. )

--- Defence1/Assets/Scripts/GameManager.cs
+++ Defence1/Assets/Scripts/GameManager.cs
@@ -8,32 +8,11 @@ public class GameManager : MonoBehaviour {
 
 	public Text oreText;
 
-	[SerializeField] float mapSize = 10f;
-
-	[SerializeField] GameObject orePrefab;
-	[SerializeField] Miner minerPrefab;
-
+	public float mapSize = 10f;
+	public GameObject orePrefab;
+	public Miner minerPrefab;
 
 	ResourceControl resourceControl;
-	[SerializeField] PreviewState _placementState = PreviewState.None;
-	Preview previewTower;
-
-	public PreviewState previewState{
-		get{ return _placementState;}
-		set{
-			if(value != _placementState){
-				_placementState = value;
-				switch(value){
-				case PreviewState.None:
-					previewTower = null;
-					break;
-				case PreviewState.Miner:
-					previewTower = makePreview (minerPrefab.gameObject);
-					break;
-				}
-			}
-		}
-	}
 
 	// Use this for initialization
 	void Start () {
@@ -42,10 +21,7 @@ public class GameManager : MonoBehaviour {
 	}
 
 	// Update is called once per frame
-	void Update () {
-		handleMousePoint ();
-		handleTowerPlacement ();
-	}
+	void Update () { }
 
 	void generateMap(){
 		var mapGen = new MapGenerator(oreNum: 50);
@@ -58,60 +34,10 @@ public class GameManager : MonoBehaviour {
 		);
 	}
 
-	Preview makePreview(GameObject prefab){
-		var obj = Instantiate (prefab);
-		destroyOptionally (obj.GetComponent<Miner> ());
-
-		var r = obj.AddComponent <Rigidbody>();
-		r.isKinematic = true;
-		var preview = obj.AddComponent <Preview>();
-
-		return preview;
-	}
-
-	void destroyOptionally(MonoBehaviour b){
-		if(b!=null){
-			Destroy (b);
-		}
-	}
-
 	public Vector2 randomPosInScene(){
 		var x = mapSize * (UnityEngine.Random.value - 0.5f);
 		var y = mapSize * (UnityEngine.Random.value - 0.5f);
 		return new Vector2 (x, y);
-	}
-
-	public void MinerButtonClicked(){
-		previewState = PreviewState.Miner;
-	}
-
-	void handleMousePoint(){
-		if (previewTower != null) {
-			var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			var plane = new Plane (Vector3.up, 0f);
-
-			float dis;
-			if (plane.Raycast (ray, out dis)) {
-				var pos = ray.GetPoint (dis);
-				previewTower.transform.position = pos;
-			}
-		}
-	}
-
-	void handleTowerPlacement() {
-		if(Input.GetButtonUp ("LeftClick") && previewTower != null && previewTower.valid){
-			
-			var pos = previewTower.transform.position;
-
-			switch(previewState){
-			case PreviewState.Miner:
-				createMiner (new Vector2(pos.x,pos.z));
-				break;
-			default:
-				throw new UnityException ("Don't know what to create!");
-			}
-
-		}
 	}
 
 	public Miner createMiner(Vector2 pos){
@@ -120,12 +46,6 @@ public class GameManager : MonoBehaviour {
 		return miner;
 	}
 
-	/// <summary>
-	/// Represent which tower will be added next.
-	/// </summary>
-	public enum PreviewState{
-		None,
-		Miner
-	}
+
 }
 
---
+++ Defence1/Assets/Scripts/UIManager.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+	public GameManager gManager;
+
+	[SerializeField] PreviewState _placementState = PreviewState.None;
+	Preview previewTower;
+
+	public PreviewState previewState{
+		get{ return _placementState;}
+		set{
+			if(value != _placementState){
+				_placementState = value;
+				switch(value){
+				case PreviewState.None:
+					Destroy (previewTower.gameObject);
+					previewTower = null;
+					break;
+				case PreviewState.Miner:
+					previewTower = makePreview (gManager.minerPrefab.gameObject);
+					break;
+				}
+			}
+		}
+	}
+
+
+	void Start () { }
+
+	void Update () {
+		handleMousePoint ();
+		handleCancelation ();
+		handleTowerPlacement ();
+	}
+
+
+	public void MinerButtonClicked(){
+		previewState = PreviewState.Miner;
+	}
+
+	Preview makePreview(GameObject prefab){
+		var obj = Instantiate (prefab);
+		destroyOptionally (obj.GetComponent<Miner> ());
+
+		var r = obj.AddComponent <Rigidbody>();
+		r.isKinematic = true;
+		var preview = obj.AddComponent <Preview>();
+
+		return preview;
+	}
+
+	void destroyOptionally(MonoBehaviour b){
+		if(b!=null){
+			Destroy (b);
+		}
+	}
+
+	void handleMousePoint(){
+		if (previewTower != null) {
+			var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			var plane = new Plane (Vector3.up, 0f);
+
+			float dis;
+			if (plane.Raycast (ray, out dis)) {
+				var pos = ray.GetPoint (dis);
+				previewTower.transform.position = pos;
+			}
+		}
+	}
+
+	void handleCancelation(){
+		if(Input.GetButtonDown ("Cancel")){
+			previewState = PreviewState.None;
+		}
+	}
+
+	void handleTowerPlacement() {
+		if(Input.GetButtonUp ("LeftClick") && previewTower != null && previewTower.valid){
+
+			var pos = previewTower.transform.position;
+
+			switch(previewState){
+			case PreviewState.Miner:
+				gManager.createMiner (new Vector2(pos.x,pos.z));
+				break;
+			default:
+				throw new UnityException ("Don't know what to create!");
+			}
+
+		}
+	}
+
+	/// <summary>
+	/// Represent which tower will be added next.
+	/// </summary>
+	public enum PreviewState{
+		None,
+		Miner
+	}
+}
