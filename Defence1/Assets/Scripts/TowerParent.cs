7fedf85e91cb0788985f173972125419eaaece98
魏家一 <wjydzh1@163.com>
2015-07-05 15:51:10 +0800

    TowerParent.

--- Defence1/Assets/Scripts/TowerParent.cs
+++ Defence1/Assets/Scripts/TowerParent.cs
@@ -1,14 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-abstract class TowerParent : MonoBehaviour {
+public abstract class TowerParent : MonoBehaviour {
 
-	public EnergyNode energyNode;
+	public EnergyNode energyNode { get; private set;} //The embeded enrgyNode for every building.
+
+	public float powerLeft = 0;
+
+	public abstract float maxPower ();
 
 	/// <summary>
 	/// Remember to call this method first in every child's init.
 	/// </summary>
 	protected void initParent(){
 		energyNode = gameObject.AddComponent <EnergyNode>();
+		energyNode.init (energyArrive);
+	}
+
+	/// <summary>
+	/// This method can be overrided by some tower, for example, 
+	/// The Power Station doesn't consume any Power.
+	/// </summary>
+	/// <returns> energy left. </returns>
+	/// <param name="amount">energy arrived.</param>
+	protected virtual float energyArrive(float amount){
+		var powerNeed = maxPower () - powerLeft;
+		var powerGet = Mathf.Min (powerNeed, amount);
+		powerLeft += powerGet;
+		return amount - powerGet;
+	}
+
+	/// <summary>
+	/// Destroy all the active part of this tower, just for preview use.
+	/// </summary>
+	public void turnToPreviewModel(){
+		Destroy (energyNode);
+		Destroy (this);
 	}
 }
