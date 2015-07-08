000e255ffac4d695a09f6380968d6af0262df1c1
魏家一 <wjydzh1@163.com>
2015-07-08 10:40:04 +0800

    Add HitpointControl and EnemyParent.

---
+++ Defence1/Assets/Scripts/HitpointControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class HitpointControl {
+	public float maxHitpoint;
+	float _hp;
+	public float hp { 
+		get{return _hp;} 
+		private set {
+			_hp = value;
+			if(value < 0){
+				outOfHp (value);
+			}
+		}
+	}
+
+	Action<float> outOfHp;
+
+	public HitpointControl(float maxHp, float initHp, Action<float> outOfHp){
+		this.outOfHp = outOfHp;
+		this.maxHitpoint = maxHp;
+		hp = initHp;
+	}
+
+}
