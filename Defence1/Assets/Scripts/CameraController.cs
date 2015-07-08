17bd8885483ae043cefb34c5ec2b09112ec20777
魏家一 <wjydzh1@163.com>
2015-07-08 21:25:40 +0800

    You can change camera height by scrolling mouse now.

--- Defence1/Assets/Scripts/CameraController.cs
+++ Defence1/Assets/Scripts/CameraController.cs
@@ -5,6 +5,8 @@ public class CameraController : MonoBehaviour {
 
 	public float moveSpeed = 5;
 	public float shiftFactor = 3;
+	public float basicHeight = 10;
+	public float scaleSpeed = 0.1f;
 
 	// Use this for initialization
 	void Start () {
@@ -16,14 +18,22 @@ public class CameraController : MonoBehaviour {
 		moveCamera ();
 	}
 
+	float scale = 0.5f;
 	void moveCamera(){
 		var h = Input.GetAxis ("Horizontal");
 		var v = Input.GetAxis ("Vertical");
 		var shift = Input.GetButton ("Shift");
+		var scroll = Input.mouseScrollDelta.y;
 
 		var speed = shift ? shiftFactor * moveSpeed : moveSpeed;
-		var delta = new Vector3 (h, 0f, v) * (speed * Time.deltaTime);
-
+		var delta = new Vector3 (h, scroll, v) * (speed * Time.deltaTime);
 		transform.position += delta;
+
+		scale = Mathf.Clamp01 (scale + scroll * scaleSpeed);
+		transform.position = new Vector3 (transform.position.x, heightOfScale (scale), transform.position.z);
+	}
+
+	float heightOfScale(float scale){
+		return basicHeight * 2f / (2f - scale);
 	}
 }
