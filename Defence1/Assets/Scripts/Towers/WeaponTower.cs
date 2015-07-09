8a673d8c7de311c8962b98f5478f1d757b8f60d3
Junyuan Hong <jyhong836@gmail.com>
2015-07-09 17:29:27 +0800

    modify some prefabs

--- Defence1/Assets/Scripts/Towers/WeaponTower.cs
+++ Defence1/Assets/Scripts/Towers/WeaponTower.cs
@@ -9,22 +9,9 @@ public enum TowerMode {
 
 public class WeaponTower : Tower {
 	
-//	[SerializeField] protected Enemy currentTarget;
 	protected bool isAttacking = false;
 
 	public AttackingControl attackControl;
-
-//	protected bool _isFiring;
-//	protected virtual bool isFiring{ get{
-//			return _isFiring;
-//		} 
-//		set{
-//			_isFiring = value;
-//			if (value) {
-//				
-//			}
-//		} 
-//	}
 
 	[SerializeField] protected float idlePowerUsage = 0.001f; // per sec
 	[SerializeField] protected float attackPowerUsage = 0.01f; // per sec
@@ -101,61 +88,6 @@ public class WeaponTower : Tower {
 			attackControl.DrawAttackLine ();
 		}
 	}
-
-//	void StartAttack () {
-//		if (currentTarget!=null)
-//			isAttacking = true;
-//		else {
-//			ChangeCurrentTarget();
-//		}
-//	}
-
-//	// control the attack time
-//	protected void Attack () {
-//		if (nextAttackTime <= 0) {
-//			nextAttackTime += AttackTarget();
-//		} else {
-//			nextAttackTime -= Time.fixedDeltaTime;
-//			isFiring = false;
-//		}
-//	}
-
-//	/// <summary>
-//	/// Attacks the target.
-//	/// </summary>
-//	/// <returns>Time interval.</returns>
-//	protected virtual float AttackTarget () {
-//		if (currentTarget == null || currentTarget.hpControl.hp <= 0 || isTargetOutOfRange) {
-//			ChangeCurrentTarget ();
-//		} else if (aimControl.ready) {
-//			isFiring = true;
-//			return attackInterval;
-//		} else 
-//			aimControl.updateOrientation (Time.fixedDeltaTime);
-//		isFiring = false;
-//		return 0;
-//	}
-
-//	/// <summary>
-//	/// Changes the current target to a new enemy.
-//	/// </summary>
-//	/// <returns>Time for rotate to new target or 0.</returns>
-//	protected virtual bool ChangeCurrentTarget () {
-//		var colliders = Physics.OverlapSphere (transform.position,attackingRadius,Masks.Enemy);
-//		if (colliders.Length > 0) {
-//			//random pick one
-//			var index = UnityEngine.Random.Range (0, colliders.Length);
-//			var enemy = colliders [index].gameObject.GetComponent <Enemy>();
-//			currentTarget = enemy;
-////			isAttacking = true;
-//
-//			return true;
-//		} else {
-//			currentTarget = null;
-////			isAttacking = false;
-//			return false;
-//		}
-//	}
 
 	#region implemented abstract members of TowerParent
 	public override float maxPower ()
